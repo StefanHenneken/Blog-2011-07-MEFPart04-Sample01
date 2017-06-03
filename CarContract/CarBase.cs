336d96c1a1de544f64a3cbc91a1c1f1d593b0ee1
Stefan Henneken <stefan.henneken@live.com>
2017-06-03 23:23:38 +0200

    Add project files.

---
+++ CarContract/CarBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.Composition;
+
+namespace CarContract
+{
+    [InheritedExport(typeof(ICarContract))]    
+    [ExportMetadata("Name", "no name")]
+    [ExportMetadata("Color", "no color")]
+    public class CarBase : ICarContract
+    {
+        public virtual string StartEngine(string name)
+        {
+            return String.Format("{0} starts the BaseCar.", name);
+        }
+    }
+}
---
+++ CarMercedes/Mercedes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CarContract;
+
+namespace CarMercedes
+{
+    public class Mercedes : CarBase
+    {
+        public override string StartEngine(string name)
+        {
+            return String.Format("{0} starts the Mercedes.", name);
+        }
+    }
+}
